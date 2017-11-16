30773cf24809309dc083f29b166751a99de22c8c
danraff <d.raff@alumni.ubc.ca>
2017-11-16 14:43:53 -0800

    Set up script

---
+++ cookie_cutter.sh
@@ -0,0 +1,10 @@
+#! /bin/bash/
+
+# Note: I've added the directory containing this script to my PATH
+# so it can be run from anywhere
+
+CURRENT_DIR="$PWD"
+
+echo $CURRENT_DIR
+
+mkdir "specific_project_name"
